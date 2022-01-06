f23713d8c1338df5d228455994a100b998e0f902
JYCabello <j.cabello.ramos@gmail.com>
2022-01-06 11:10:23 +0100

    Copied class

--- fp-primitive-obsession/src/PrimitiveObsession/PrimitiveObsession/Library.fs
+++ fp-primitive-obsession/src/PrimitiveObsession/PrimitiveObsession/Library.fs
@@ -1,5 +1,41 @@
 ﻿namespace PrimitiveObsession
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+type ProfitCalculator(localCurrency: string) =
+  let rates =
+    [ ("GBP", 1.0)
+      ("USD", 1.6)
+      ("EUR", 1.2) ]
+    |> Map.ofSeq
+
+  let mutable localAmount = 0
+  let mutable foreignAmount = 0
+
+  do
+    try
+      rates.[localCurrency] |> ignore
+    with
+    | _ -> invalidArg (nameof localCurrency) "Was not a valid currency"
+
+  member _.add amount currency incoming =
+    let mutable realAmount: int = amount
+
+    let exchangeRate =
+      rates.TryFind currency
+      |> Option.map (fun incomingRate -> incomingRate / rates.[localCurrency])
+
+    realAmount <-
+      exchangeRate
+      |> Option.map (fun rate -> ((realAmount |> float) / rate) |> int)
+      |> Option.defaultValue realAmount
+
+    if incoming then
+      do realAmount <- -realAmount
+
+    if localCurrency = currency then
+      do localAmount <- localAmount + realAmount
+    else
+      do foreignAmount <- foreignAmount + realAmount
+
+  member _.calculateTax = if localAmount < 0 then 0 else ((localAmount |> float) * 0.2) |> int
+
+  member this.calculateProfit = localAmount - this.calculateTax + foreignAmount
