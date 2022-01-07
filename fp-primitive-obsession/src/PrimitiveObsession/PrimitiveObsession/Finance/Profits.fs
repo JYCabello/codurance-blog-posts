d696ea1ae1b3b85e72f8050f876eb5fe52b6adb8
JYCabello <j.cabello.ramos@gmail.com>
2022-01-07 21:06:12 +0100

    Finished file extraction

--- fp-primitive-obsession/src/PrimitiveObsession/PrimitiveObsession/Finance/Profits.fs
+++ fp-primitive-obsession/src/PrimitiveObsession/PrimitiveObsession/Finance/Profits.fs
@@ -1,27 +1,5 @@
 ﻿namespace PrimitiveObsession.Finance
 
-
-module Taxes =
-  open Accounting
-  open Trading
-
-  // It became obvious that the point of these functions was to get amounts to
-  // be taxed or not.
-  let taxableAmount balance =
-    amount
-      balance.LocalCurrency
-      (balance.Transactions
-       |> transactionList
-       |> List.filter (isIn balance.LocalCurrency))
-
-  let taxFreeAmount balance =
-    amount
-      balance.LocalCurrency
-      (balance.Transactions
-       |> transactionList
-       |> List.filter (isNotIn balance.LocalCurrency))
-
-
 module Profits =
   open Accounting
   open Trading
