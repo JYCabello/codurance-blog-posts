718e8d6511a6c004857dbc3480cdd619963fddb0
JYCabello <j.cabello.ramos@gmail.com>
2022-01-07 21:03:07 +0100

    Another file extraction

---
+++ fp-primitive-obsession/src/PrimitiveObsession/PrimitiveObsession/Finance/Accounting.fs
@@ -0,0 +1,23 @@
+﻿namespace PrimitiveObsession.Finance
+
+module Accounting =
+  open Currencies
+  open Trading
+
+  // The aforementioned entity, which will replace the Items collection suggested in the excercise.
+  type Balance =
+    { Transactions: Transactions
+      LocalCurrency: Currency }
+
+  let isIn currency transaction =
+    transaction
+    |> money
+    |> fun money -> money.Currency = currency
+
+  let isNotIn currency = isIn currency >> not
+
+  let amount currency transactions =
+    let aggregate acc trx = acc + (trx |> money)
+
+    ({ Amount = 0; Currency = currency }, transactions)
+    ||> List.fold aggregate
--- fp-primitive-obsession/src/PrimitiveObsession/PrimitiveObsession/Library.fs
+++ fp-primitive-obsession/src/PrimitiveObsession/PrimitiveObsession/Library.fs
@@ -1,27 +1,5 @@
 ﻿namespace PrimitiveObsession.Finance
 
-module Accounting =
-  open Currencies
-  open Trading
-
-  // The aforementioned entity, which will replace the Items collection suggested in the excercise.
-  type Balance =
-    { Transactions: Transactions
-      LocalCurrency: Currency }
-
-  let isIn currency transaction =
-    transaction
-    |> money
-    |> fun money -> money.Currency = currency
-
-  let isNotIn currency = isIn currency >> not
-
-  let amount currency transactions =
-    let aggregate acc trx = acc + (trx |> money)
-
-    ({ Amount = 0; Currency = currency }, transactions)
-    ||> List.fold aggregate
-
 
 module Taxes =
   open Accounting
